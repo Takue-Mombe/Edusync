71bc175e45a86220e67f042a0e579a3aef26a829
mombe <h220462g@hit.ac.zw>
2024-03-25 13:59:31 +0200

    thymeleaf implemented in the courses Table

---
+++ src/main/java/org/projecttherevelation/edusync/Configs/AppConfig.java
@@ -0,0 +1,16 @@
+package org.projecttherevelation.edusync.Configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
+}
