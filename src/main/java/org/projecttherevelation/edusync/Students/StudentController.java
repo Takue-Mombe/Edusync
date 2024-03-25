871b6b512277eba2556d42775790ed738999f7b2
mombe <h220462g@hit.ac.zw>
2024-03-25 14:38:40 +0200

    adding a modal to the courses and students html

--- src/main/java/org/projecttherevelation/edusync/Students/StudentController.java
+++ src/main/java/org/projecttherevelation/edusync/Students/StudentController.java
@@ -1,13 +1,17 @@
 package org.projecttherevelation.edusync.Students;
 
 
+import org.projecttherevelation.edusync.Courses.CourseService;
+import org.projecttherevelation.edusync.Courses.CoursesModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/StudentApi")
 public class StudentController {
 
@@ -29,6 +33,18 @@ public class StudentController {
     public List<StudentModel> find(){
         return studentService.findAll();
     }
+    @GetMapping("/students/all")
+    public String getAllStudents(Model model) {
+        // Retrieve all student data from the database
+        List<StudentModel> students = studentService.findAll();
+
+        // Add the list of students to the model
+        model.addAttribute("students", students);
+
+        // Return the name of your Thymeleaf template
+        return "student-list";
+    }
+
     @GetMapping("/getById/{hitMail}")
     public Optional<StudentModel>findById(@PathVariable String hitMail){
         return studentService.findById(hitMail);
